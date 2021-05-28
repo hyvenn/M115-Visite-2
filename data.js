b3fc880e09077fd31e972bc2c017f88c0cf51c8f
hyvenn <56298319+hyvenn@users.noreply.github.com>
2021-05-28 10:24:27 +0200

    visite 2

---
+++ data.js
@@ -0,0 +1,413 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r-5-accueil",
+      "name": "R 5 accueil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.34979683023003005,
+        "pitch": 0.06789237791024405,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7805760082157995,
+          "pitch": 0.024512540812413164,
+          "rotation": 0,
+          "target": "1-r-5-bureaux"
+        },
+        {
+          "yaw": 1.537993210789507,
+          "pitch": -0.3710214850598028,
+          "rotation": 0,
+          "target": "3-r-6-bureaux"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r-5-bureaux",
+      "name": "R 5 bureaux",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1458348363754265,
+          "pitch": 0.07337875453183429,
+          "rotation": 0,
+          "target": "0-r-5-accueil"
+        },
+        {
+          "yaw": 1.7245117567579689,
+          "pitch": -0.08363373652784567,
+          "rotation": 5.497787143782138,
+          "target": "7-r-6-circulation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r-6-bureau-serge",
+      "name": "R 6 bureau Serge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6071412730059684,
+          "pitch": 0.06507188583153933,
+          "rotation": 0,
+          "target": "7-r-6-circulation"
+        },
+        {
+          "yaw": -3.1226892175583014,
+          "pitch": 0.020366161632262347,
+          "rotation": 0,
+          "target": "4-r-6-reunion"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r-6-bureaux",
+      "name": "R 6 bureaux",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.06223531719273,
+          "pitch": 0.05507471445709555,
+          "rotation": 4.71238898038469,
+          "target": "7-r-6-circulation"
+        },
+        {
+          "yaw": 2.0386846633079374,
+          "pitch": 0.03856098146523301,
+          "rotation": 1.5707963267948966,
+          "target": "7-r-6-circulation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-r-6-reunion",
+      "name": "R 6 reunion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0215974299703241,
+          "pitch": 0.06826105891170542,
+          "rotation": 0,
+          "target": "2-r-6-bureau-serge"
+        },
+        {
+          "yaw": -1.8905881035382794,
+          "pitch": 0.06579965254192643,
+          "rotation": 0,
+          "target": "7-r-6-circulation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-r-7-cuisine",
+      "name": "R 7 cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2674171712231228,
+          "pitch": 0.06823206047215713,
+          "rotation": 0,
+          "target": "6-r-7-terrasse"
+        },
+        {
+          "yaw": 0.002345750502257715,
+          "pitch": 0.4030496670988448,
+          "rotation": 2.356194490192345,
+          "target": "7-r-6-circulation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-r-7-terrasse",
+      "name": "R 7 terrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0624638048990587,
+          "pitch": 0.10423323452329214,
+          "rotation": 0,
+          "target": "5-r-7-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-r-6-circulation",
+      "name": "R 6 circulation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5790513340392671,
+          "pitch": 0.040138369292060716,
+          "rotation": 0.7853981633974483,
+          "target": "2-r-6-bureau-serge"
+        },
+        {
+          "yaw": 2.6532878051114066,
+          "pitch": 0.010125449805812536,
+          "rotation": 5.497787143782138,
+          "target": "4-r-6-reunion"
+        },
+        {
+          "yaw": 1.894594551974861,
+          "pitch": 0.936690666843699,
+          "rotation": 2.356194490192345,
+          "target": "0-r-5-accueil"
+        },
+        {
+          "yaw": 1.7003565583407951,
+          "pitch": -0.8959951642243364,
+          "rotation": 5.497787143782138,
+          "target": "5-r-7-cuisine"
+        },
+        {
+          "yaw": -0.40106793653768946,
+          "pitch": 0.07825990042626252,
+          "rotation": 10.995574287564278,
+          "target": "3-r-6-bureaux"
+        },
+        {
+          "yaw": -2.8251546894703345,
+          "pitch": 0.08851836957882497,
+          "rotation": 1.5707963267948966,
+          "target": "3-r-6-bureaux"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "M115 Visite 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
